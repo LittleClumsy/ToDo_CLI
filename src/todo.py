5e40d0a0849552c374dc2b53f43d08dc555252d1
LittleClumsy <Bengelbrecht2002@gmail.com>
2023-03-27 17:57:35 +0200

    Create config folder

--- src/todo.py
+++ src/todo.py
@@ -1,2 +1,32 @@
-print("Hello World")
-print("Thank goodness we avoided merge conflicts.")
+import json
+import os
+from os.path import expanduser
+import platform
+
+
+home = os.path.expanduser('~')
+print(home)
+
+
+folder_name = ".todo"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+    print("Folder created:", folder_name)
+else:
+    print("Folder already exists:", folder_name)
+
+
+
+
+folder_path = os.path.join(os.path.expanduser("~"), folder_name)
+file_path = os.path.join(folder_path, "tasks.json")
+
+
+
+# Write the JSON data to the file
+data = {"test": "Hi","age":30}
+with open(file_path, "w") as f:
+    json.dump(data, f)
+
+
+
