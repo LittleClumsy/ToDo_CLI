fc821aa9a44c06927b1653f570717d5c2b07c82d
Evanlab02 <evanlabuschagne70@gmail.com>
2023-05-16 14:47:13 +0200

    Small Config Controller Refactor

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -2,9 +2,16 @@
 This module contains unit tests for the config module.
 """
 from json import load
-from os import remove, removedirs
 
-from todo_cli.config.config_controller import install_config_file, read_config_file
+from pytest import raises
+
+from tests.unit.test_helpers import remove_test_files
+
+from todo_cli.config.config_controller import (
+    install_config_file,
+    read_config_file,
+    write_config_file
+)
 from todo_cli.helpers.os_helper import get_config_directory
 
 
@@ -12,13 +19,15 @@ def test_install_config_file():
     """
     Tests that the install config file function works as expected.
     """
-    install_config_file()
+    expected = {}
+    installed_config_file = install_config_file()
+
     with open(f"{get_config_directory()}/config.json", "r", encoding="UTF-8") as file:
         actual = load(file)
-    expected = {}
+
+    assert installed_config_file is True
     assert actual == expected
-    remove(f"{get_config_directory()}/config.json")
-    removedirs(get_config_directory())
+    remove_test_files()
 
 
 def test_read_config_file():
@@ -26,6 +35,66 @@ def test_read_config_file():
     This will test the read config file function.
     """
     expected = {}
-    install_config_file()
+    installed_config_file = install_config_file()
     actual = read_config_file()
+    assert installed_config_file is True
     assert actual == expected
+    remove_test_files()
+
+
+def test_double_install_config_file():
+    """
+    This will test that the install config file function works as expected when
+    the file already exists.
+    """
+    installed_config_file = install_config_file()
+    installed_config_file_again = install_config_file()
+    assert installed_config_file is True
+    assert installed_config_file_again is False
+    remove_test_files()
+
+
+def test_read_config_file_not_found():
+    """
+    This will test that the read config file function works as expected when
+    the file does not exist.
+    """
+    with raises(FileNotFoundError):
+        read_config_file()
+
+
+def test_read_config_file_invalid_json():
+    """
+    This will test that the read config file function works as expected when
+    the file is not valid json.
+    """
+    installed_config_file = install_config_file()
+    with open(f"{get_config_directory()}/config.json", "w", encoding="UTF-8") as file:
+        file.write("invalid json")
+    with raises(ValueError):
+        read_config_file()
+    remove_test_files()
+    assert installed_config_file is True
+
+
+def test_write_config_file():
+    """
+    This will test that the write config file function works as expected.
+    """
+    expected = {"key": "value"}
+    installed_config_file = install_config_file()
+    write_config_file(expected)
+    actual = read_config_file()
+    assert installed_config_file is True
+    assert actual == expected
+    remove_test_files()
+
+
+def test_write_config_file_with_no_directory():
+    """
+    This will test that the write config file function works as expected when
+    the directory does not exist.
+    """
+    expected = {"key": "value"}
+    with raises(FileNotFoundError):
+        write_config_file(expected)
--- todo_cli/config/config_controller.py
+++ todo_cli/config/config_controller.py
@@ -2,43 +2,75 @@
 This module will contain all logic pertaining to the configuration of the application.
 """
 
+# File Imports
 from todo_cli.helpers.json_helper import write_json_file, read_json_file
 from todo_cli.helpers.os_helper import create_directory, get_config_directory, path_exists
 
 
-def install_config_file():
+def install_config_file() -> bool:
     """
     This will install the config file for the application.
+    The config file will be located at ~/.todo/config.json.
+    If the file already exists, it will not be overwritten.
+
+    Returns:
+        bool: True if the file was created, False if the file already exists.
+
+    Examples:
+        >>> install_config_file()
+        True
+
+        >>> install_config_file()
+        False
     """
     config_directory = get_config_directory()
     if not path_exists(f"{config_directory}/config.json"):
         create_directory(config_directory)
         write_json_file(f"{config_directory}/config.json", {})
+        return True
+    return False
 
 
-def read_config_file() -> str:
+def read_config_file() -> dict | list:
     """
     This function will read the json file contents.
 
     Returns:
-        str: Contents of .json file.
+        dict | list: The contents of the json file.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        ValueError: If the file is not valid json.
 
     Examples:
         >>> read_config_file()
-        '/home/username/.todo/config.json.
+        {"key": "value"}
     """
-    config_directory = get_config_directory()
-    file_path = f"{config_directory}/config.json"
-    config = read_json_file(file_path)
+    try:
+        config_directory = get_config_directory()
+        file_path = f"{config_directory}/config.json"
+        config = read_json_file(file_path)
+    except FileNotFoundError as file_not_found_error:
+        raise FileNotFoundError(
+            f"Config file not found at {file_path}.") from file_not_found_error
+    except ValueError as value_error:
+        raise ValueError(
+            f"Config file at {file_path} is not valid json.") from value_error
     return config
 
 
-def write_config_file(content: dict):
+def write_config_file(content: dict) -> None:
     """
     This function will write to the config file.
 
     Args:
         content (dict): Content to write to config file.
+
+    Raises:
+        FileNotFoundError: If the directory does not exist.
+
+    Examples:
+        >>> write_config_file({"key": "value"})
     """
     config_directory = get_config_directory()
     file_path = f"{config_directory}/config.json"
