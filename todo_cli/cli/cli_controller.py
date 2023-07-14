ed92694f1a45f994ac667ca237b7cbc071790cd6
LittleClumsy <Bengelbrecht2002@gmail.com>
2023-07-14 09:53:08 +0200

    Added edit function

--- todo_cli/cli/cli_controller.py
+++ todo_cli/cli/cli_controller.py
@@ -1,8 +1,8 @@
 """
 This module contains all logic pertaining to the CLI commands
 """
-import typer
 import uuid
+import typer
 from typing_extensions import Annotated
 from todo_cli.helpers.tasks_helper import (
     read_tasks_file,
@@ -39,7 +39,7 @@ def view() -> None:
 
 @app.command()
 def edit(
-    id: Annotated[str, typer.Option(prompt=True)],
+    task_id: Annotated[str, typer.Option(prompt=True)],
     field_name: Annotated[str, typer.Option(prompt=True)],
     value: Annotated[str, typer.Option(prompt=True)]
 ) -> None:
@@ -54,18 +54,18 @@ def edit(
     if field_name == "UUID":
         print("Invalid field. The UUID can not be edited.")
         raise typer.Exit(code=3)
-    elif field_name != "name" and field_name != "date":
+    if field_name not in ('name', 'date'):
         print("That field does not exist! Please select a valid field to edit.")
         raise typer.Exit(code=3)
 
     content = read_tasks_file()
     for task in content:
-        if task['UUID'] == id:
+        if task['UUID'] == task_id:
             task[field_name] = value
     write_tasks_file(content)
 
 
-def adding_content(task_uuid: str, name: str, date: str):
+def adding_content(task_id: str, name: str, date: str):
     """
     This function will handle adding a task to the tasks.json file
 
@@ -74,6 +74,6 @@ def adding_content(task_uuid: str, name: str, date: str):
         date(str): The date
     """
     tasks_data = read_tasks_file()
-    new_task = create_task(task_uuid, name, date)
+    new_task = create_task(task_id, name, date)
     tasks_data.append(new_task)
     write_tasks_file(tasks_data)
